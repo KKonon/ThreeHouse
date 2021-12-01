b01e3733b1abdb1d09fa6a32c06ad13d27a2c203
Kirill Kononovich <kkononovich26@gmail.com>
2021-12-01 22:00:01 +0300

    Create nav-header

---
+++ php/nav.php
@@ -0,0 +1,16 @@
+<section class="nav content">
+
+    <div class="logo">
+        <div class="img"></div>
+        <h2>Threehouse</h2>
+    </div>
+
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+
+</section>
