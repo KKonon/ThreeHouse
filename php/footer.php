24ee35dd08d21dff89512c12bb1dac48fd7f308c
Kirill Kononovich <kkononovich26@gmail.com>
2021-12-02 21:28:41 +0300

    footer 2

--- php/footer.php
+++ php/footer.php
@@ -27,8 +27,44 @@
 
     </div>
 
-    <div class="posts">
-        
+    <div class="posts content">
+        <h3>Latest Posts</h3>
+
+        <div class="news1">
+            <div class="img__new1"></div>
+            <div class="news1__p">
+                <p>Made With Love In Toronto</p>
+            </div>
+        </div>
+
+        <div class="img">
+            <div class="img__img1"></div>
+            <div class="img1__p">
+                <p>Startap News & Emerging Tech</p>
+            </div>
+        </div>
+
+        <div class="news2">
+            <div class="img__new2"></div>
+            <div class="news2__p">
+                <p>Bitcoin Will Soon Rule The World</p>
+            </div>
+        </div>
+
+        <div class="news3">
+            <div class="img__new3"></div>
+            <div class="news3__p">
+                <p>Werable Technology On The Rise</p>
+            </div>
+        </div>
+
+        <div class="media">
+            <div class="img__media"></div>
+            <div class="media__p">
+                <p>Learn Web Design In 30 Days!</p>
+            </div>
+        </div>
+
     </div>
 
     <div class="tweets">
