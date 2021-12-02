003ad3c2e7a794542200555ff16ab9e85070c6bd
Kirill Kononovich <kkononovich26@gmail.com>
2021-12-02 21:04:42 +0300

    footer 1

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,40 @@
-<footer>footer</footer>
+<footer class="footer">
+
+    <div class="contact content">
+
+        <h3>Contact us</h3>
+
+        <div class="map">
+            <div class="map__img"></div>
+            <div class="map__p">
+                <p>55 Main St. Toronto, ON M1H 3A5</p>
+            </div>
+        </div>
+
+        <div class="phone">
+            <div class="phone__img"></div>
+            <div class="phone__p">
+                <p>(416) 555-5252</p>
+            </div>
+        </div>
+
+        <div class="mail">
+            <div class="mail__img"></div>
+            <div class="mail__p">
+                <p>hellohi@gmail.com</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="posts">
+        
+    </div>
+
+    <div class="tweets">
+
+    </div>
+
+</footer>
 </body>
 </html>
