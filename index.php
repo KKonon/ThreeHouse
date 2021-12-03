4ece07553e9769870b0d808f1c9b258fe1fb1f8b
Kirill Kononovich <kkononovich26@gmail.com>
2021-12-03 21:50:48 +0300

    proffesional finish

--- index.php
+++ index.php
@@ -5,11 +5,43 @@ require_once "./php/nav.php";
 ?>
 
 <div class="head">
+
     <h1>Creative Digital <br>Solutions</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
 
     <div class="head__btn">
         <button>View Portfolio</button>
+    </div>
+
+</div>
+
+<div class="proffesional">
+
+    <div class="prof__head">
+        <h2>Professional services</h2>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+    </div>
+
+    <div class="prof_wrapper content">
+
+        <div class="prof__design">
+            <div class="prof__design__img"></div>
+            <h3>Beautiful Designs</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>Nemo recusandae voluptates, commodi.</p>
+        </div>
+
+        <div class="prof__web">
+            <div class="prof__web__img"></div>
+            <h3>Responsive Web</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>Nemo recusandae voluptates, commodi.</p>
+        </div>
+
+        <div class="prof__tools">
+            <div class="prof__tools__img"></div>
+            <h3>Marketing Tools</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>Nemo recusandae voluptates, commodi.</p>
+        </div>
+
     </div>
 </div>
 
