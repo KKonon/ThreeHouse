797dc64e99f9338fcfba6385658f9c34ce088021
Kirill Kononovich <kkononovich26@gmail.com>
2021-12-06 21:49:23 +0300

    finish all

--- php/project.php
+++ php/project.php
@@ -1,29 +1,40 @@
 <div class="project">
-    <div class="project__title">
+    <div class="project__title content">
         <h2>Latest project</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit</p>
     </div>
 
-    <div class="project__development">
+    <div class="project__development content">
         <h3>Developing framework</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod architecto tenetur ratione similique, magni esse, molestiae pariatur iste.</p>
 
         <div class="project__development__basket">
             <div class="development__img1"></div>
-            <h3>E-commerce ready</h3>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
+            <div class="basket_title">
+                <h3>E-Commerce Ready</h3>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
         </div>
 
-        <div class="project__development__basket2">
-            <div class="development__img2"></div>
-            <h3>Powerfull SEO features</h3>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
+        <div class="project__development__SEO">
+            <div class="SEO__img"></div>
+            <div class="SEO_title">
+                <h3>Powerfull SEO Features</h3>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
         </div>
 
-        <div class="project__development__basket3">
-            <div class="development__img3"></div>
-            <h3>Secure Encryption</h3>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
+        <div class="project__development__Secure">
+            <div class="Secure__img"></div>
+            <div class="Secure_title">
+                <h3>Secure Encryption</h3>
+                <p>Lorem ipsum dolor sit amet consectetur</p>
+            </div>
         </div>
+    </div>
+
+    <div class="project_footer content">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat distinctio est totam modi voluptatibus eius fugiat vero incidunt hic doloribus, itaque autem neque sint voluptas, placeat perspiciatis pariatur. Illo, possimus!</p>
+        <a href="/" alt="#">View Details<img src="/images/Arrow-Right.png" /></a>
     </div>
 </div>
